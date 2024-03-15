fa48b073a4b9a81dcab68ef6a692d74a76967f5d
Cathbhadh <36742410+Cathbhadh@users.noreply.github.com>
2024-03-15 13:26:58 +0200

    Update daily.py

--- daily.py
+++ daily.py
@@ -55,7 +55,7 @@ def count_stats(data):
 
 def main():
     st.title("Post Analytics")
-    max_posts = 100000
+    max_posts = 86500
     offsets = range(0, max_posts, 500)
     all_data = fetch_and_concat_data(offsets)
 
